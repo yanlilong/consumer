1ebc735785f211a30cf162dbc4286dd2eaf7b921
yanlilong <yan.w.li@outlook.com>
2020-08-23 22:38:28 +0200

    update kafka consumer listener

--- src/main/java/com/yanlilong/event/kafka/consumer/ConsumerApplication.java
+++ src/main/java/com/yanlilong/event/kafka/consumer/ConsumerApplication.java
@@ -2,18 +2,24 @@ package com.yanlilong.event.kafka.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableKafka
 public class ConsumerApplication {
 	@RequestMapping("/")
 	public String home() {
 		return "Hello Docker World";
 	}
 	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
+
+		ConfigurableApplicationContext context=SpringApplication.run(ConsumerApplication.class, args);
+		KafkaConsumer kafkaConsumer=context.getBean(KafkaConsumer.class);
+
 	}
 
 }
--- src/main/java/com/yanlilong/event/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/yanlilong/event/kafka/consumer/KafkaConsumer.java
@@ -1,20 +1,25 @@
 package com.yanlilong.event.kafka.consumer;
 import com.yanlilong.docrobot.kafka.model.NodeEvent;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class KafkaConsumer {
-  /**  private final static Logger LOGGER = Logger.getLogger(KafkaConsumer.class);
-    @KafkaListener(topics = "${kafka.topic}", groupId="${kafka.group}",containerFactory = "nodeEventKafkaListenerFactory")
-    public void consumerJson(NodeEvent nodeEvent){
-        if (nodeEvent.getContentType().equals("F:cm:systemfolder") ||
+    private final static Logger LOGGER = Logger.getLogger(KafkaConsumer.class);
+    @KafkaListener(topics = "alfresco-nodes-event")
+    public void listen(ConsumerRecord<String, String> record){
+        System.out.println("message key: " +record.key()+" value: "+record.value());
+
+       /** if (nodeEvent.getContentType().equals("F:cm:systemfolder") ||
                 nodeEvent.getContentType().equals("F:bpm:package") ||
                 nodeEvent.getContentType().equals("I:act:actionparameter") ||
                 nodeEvent.getContentType().equals("I:act:action") ||
                 nodeEvent.getContentType().equals("D:cm:thumbnail")) {
             return;
-        }
-        LOGGER.info("Event: " + nodeEvent.getEventType() + " on " + nodeEvent.getNodeRef());
+        }*/
+        LOGGER.info("Event: " + record.key()+"value"+record.value() );
 
-    }*/
+    }
 }
